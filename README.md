d951a58feabeef1eda665e5bd253562d3cd6cdb8
yasir2000 <yasirkaram@gmail.com>
2022-01-09 18:32:50 +0300

    sixth commit

--- README.md
+++ README.md
@@ -14,7 +14,10 @@ This is a Golang project and includes :
 - Logger and defer-panic-recover middlewares 
 - Dashboard of content management system "CMS"
 - Service to capture usage analytics
-- User Authentication service to authenticate requests 
+- User Authentication service to authenticate requests
+  - Username and Password login encryption using bcrypt (scrypt) package
+  - Saving Sessions and Cookies using bot package (gorilla sessions) into DB
+  - OAuth, JWT Token authentication using jwt-go, oauth2 (sessionless or passwordless authentication)
 - Data RESTFul API 
 - Real-time chat and notification services for visitor help 
 - CI/CD automated deployment service
--- cms/users/cmd/main.go
+++ cms/users/cmd/main.go
@@ -3,12 +3,49 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 	"yasir2000/go-web-dev-side-project-1/cms/users"
 )
 
+const loginTemplate = `
+<h1>Enter your username and password</h1>
+<form action="/" method="POST">
+	<input type="text" name="user" required>
+
+	<label for="password">Password</label>
+	<input type="password" name="password" required>
+
+	<input type="submit" value="Submit">
+</form>
+`
+
+func authHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		t, _ := template.New("login").Parse(loginTemplate)
+		t.Execute(w, nil)
+	case "POST":
+		user := r.FormValue("user")
+		pass := r.FormValue("password")
+		err := users.AuthenticateUser(user, pass)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		users.SetSession(w, user)
+		w.Write([]byte("Signed in successfully"))
+	}
+
+}
+
+func restrictedHandler(w http.ResponseWriter, r *http.Request) {
+	user := users.GetSession(w, r)
+	w.Write([]byte(user))
+}
+
 func main() {
 
-	username, password := "akarama1932", "querty123"
+	username, password := "akarama1933", "querty123"
 
 	//username, password := os.Getenv("GMAIL_USERNAME"), "qwerty123"
 
@@ -18,20 +55,23 @@ func main() {
 		return
 	}
 
-	err = users.AuthenticateUser(username, password)
-	if err != nil {
-		fmt.Printf("Couldn't authenticate user : %s\n", err.Error())
-		return
-	}
+	// err = users.AuthenticateUser(username, password)
+	// if err != nil {
+	// 	fmt.Printf("Couldn't authenticate user : %s\n", err.Error())
+	// 	return
+	// }
 
-	fmt.Println("Successfuly created and authenticated user %s", username)
+	//	fmt.Println("Successfuly created and authenticated user %s", username)
+	fmt.Printf("Succesfully created and authenticated user \033[32m%s\033[0m\n", username)
 
 	// Send reset email
-	err = users.SendPasswordResetEmail(username)
-	if err != nil {
-		fmt.Println(err)
-	}
+	// err = users.SendPasswordResetEmail(username)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 
-	http.HandleFunc("/reset", users.ResetPassword)
+	//http.HandleFunc("/reset", users.ResetPassword)
+	http.HandleFunc("/", authHandler)
+	http.HandleFunc("/restricted", restrictedHandler)
 	http.ListenAndServe(":3000", nil)
 }
